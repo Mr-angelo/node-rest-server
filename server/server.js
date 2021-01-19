5e2f8b9f3f023f969280750b024347277d431538
Mr-angelo <angelcarrillo107@gmail.com>
2021-01-19 12:55:45 -0600

    Segundo commit

--- server/server.js
+++ server/server.js
@@ -1,7 +1,41 @@
-const express = require('express')
-const app = express()
+//se importan los paquetes
+require('./config/config');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/', function(req, res) {
-    res.send('Hello World')
-})
-app.listen(3000)
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+//se indica el servicio que escuchara el servidor
+app.get('/usuario', function(req, res) {
+    res.json('get usuario')
+});
+
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+    res.json({
+        persona: body
+    });
+});
+
+app.put('/usuario', function(req, res) {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete usuario')
+});
+
+
+//se indica dónde escuchara el servidpr
+app.listen(process.env.PORT, () => {
+    console.log('SERVIDOR INICIADO!!');
+});
