39ea58b4ec0fa02937cc698a1e7d4abbc7609091
Mr-angelo <angelcarrillo107@gmail.com>
2021-01-28 19:42:49 -0600

    Metodo Put

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const Usuario = require('../models/usuario');
 const bcrypt = require('bcrypt');
+const _ = require('underscore');
 
 
 //inicilización de express
@@ -41,11 +42,25 @@ app.post('/usuario', function(req, res) {
     });
 });
 
-app.put('/usuario', function(req, res) {
+app.put('/usuario/:id', function(req, res) {
     let id = req.params.id;
-    res.json({
-        id
-    });
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+
+    })
 });
 
 app.delete('/usuario', function(req, res) {
