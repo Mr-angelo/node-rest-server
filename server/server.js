cccb15228c5e4ac05347ae2a13111e351891a8bb
Mr-angelo <angelcarrillo107@gmail.com>
2021-01-29 22:26:12 -0600

    Conectado a mongo atlas

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 app.use(require('./routes/usuario'));
 
 //conexión a mongodb
-mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+mongoose.connect(process.env.URLDB, (err, res) => {
 
     if (err) throw (err);
     console.log('Base de datos activada');
